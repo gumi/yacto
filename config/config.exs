93c07eaa527dd9b080cc2ae1d2e3d4bbe0f49172
melpon <shigemasa7watanabe+github@gmail.com>
2019-10-21 11:29:43 +0900

    コスメ

--- config/config.exs
+++ config/config.exs
@@ -12,35 +12,35 @@ config :yacto,
   ]
 
 config :yacto, Yacto.XATest.Repo0,
-  database: "yact_xa_repo0",
+  database: "yacto_xa_repo0",
   username: "root",
   password: "",
   hostname: "localhost",
   port: 3306
 
 config :yacto, Yacto.XATest.Repo1,
-  database: "yact_xa_repo1",
+  database: "yacto_xa_repo1",
   username: "root",
   password: "",
   hostname: "localhost",
   port: 3306
 
 config :yacto, Yacto.QueryTest.Repo.Default,
-  database: "yact_query_repo_default",
+  database: "yacto_query_repo_default",
   username: "root",
   password: "",
   hostname: "localhost",
   port: 3306
 
 config :yacto, Yacto.QueryTest.Repo.Player0,
-  database: "yact_query_repo_player0",
+  database: "yacto_query_repo_player0",
   username: "root",
   password: "",
   hostname: "localhost",
   port: 3306
 
 config :yacto, Yacto.QueryTest.Repo.Player1,
-  database: "yact_query_repo_player1",
+  database: "yacto_query_repo_player1",
   username: "root",
   password: "",
   hostname: "localhost",
